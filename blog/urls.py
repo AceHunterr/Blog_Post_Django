4f8530fc54fb8ba6aee2cb9b1e75a7a5be6dffb4
MehulAgg26 <21BMA003@nith.ac.in>
2022-07-04 21:19:28 +0530

    Added admin models

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+# from django import views
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.starting_page, name="starting-page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug:slug>", views.post_details, name="post-detail-page"),
+]
